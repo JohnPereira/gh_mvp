7aa3d76810a56981dfdb8f599c673398eb895d6a
John Pereira <john.epm1995@gmail.com>
2018-03-30 18:31:44 -0300

    vista create account casi lista

--- app/src/main/java/com/gashere/gashere/LoginActivity.java
+++ app/src/main/java/com/gashere/gashere/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.gashere.gashere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.gashere.gashere.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView txtRegister = (TextView)findViewById(R.id.txtRegister);
+        txtRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.txtRegister:
+                //intent(donde estoy, a donde voy)
+                Intent intent = new Intent(this, CreateAccountActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/gashere/gashere/view/CreateAccountActivity.java
+++ app/src/main/java/com/gashere/gashere/view/CreateAccountActivity.java
@@ -2,6 +2,8 @@ package com.gashere.gashere.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 import com.gashere.gashere.R;
 
@@ -11,5 +13,16 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_createaccount),true);
+
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //titulo del actionbar
+        getSupportActionBar().setTitle(tittle);
+        //setear si tiene boton de regreso o no
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
