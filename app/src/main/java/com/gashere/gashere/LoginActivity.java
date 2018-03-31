8f1a8651b79e1af166b3b476d3ea1a1c2b86fa02
John Pereira <john.epm1995@gmail.com>
2018-03-31 14:11:11 -0300

    Navegation drawer recien levantado

--- app/src/main/java/com/gashere/gashere/LoginActivity.java
+++ app/src/main/java/com/gashere/gashere/LoginActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.gashere.gashere.view.ContainerActivity;
 import com.gashere.gashere.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,7 +17,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView txtRegister = (TextView)findViewById(R.id.txtRegister);
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
         txtRegister.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
     }
 
     @Override
@@ -23,8 +27,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()){
             case R.id.txtRegister:
                 //intent(donde estoy, a donde voy)
-                Intent intent = new Intent(this, CreateAccountActivity.class);
-                startActivity(intent);
+                Intent intentRegister = new Intent(this, CreateAccountActivity.class);
+                startActivity(intentRegister);
+                break;
+            case R.id.btnLogin:
+                Intent intentContainer = new Intent(this, ContainerActivity.class);
+                startActivity(intentContainer);
                 break;
         }
     }
