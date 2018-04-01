c3e7d3106a054dda40b4f258077e978f5bb9a21c
John Pereira <john.epm1995@gmail.com>
2018-04-01 14:16:35 -0300

    conexion con spring web socket realizada

--- app/src/main/java/com/gashere/gashere/view/ResponseDriverActivity.java
+++ app/src/main/java/com/gashere/gashere/view/ResponseDriverActivity.java
@@ -1,18 +1,35 @@
 package com.gashere.gashere.view;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.gashere.gashere.R;
 import com.gashere.gashere.adapter.DriverResponseAdapter;
 import com.gashere.gashere.model.Driver;
 
+
+import org.java_websocket.WebSocket;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.client.StompClient;
 
 public class ResponseDriverActivity extends AppCompatActivity {
+
+    private StompClient mStompClient;
+    RecyclerView driversRecycler;
+    DriverResponseAdapter driverResponseAdapter;
+    ArrayList<Driver> drivers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +40,57 @@ public class ResponseDriverActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        RecyclerView driversRecycler = (RecyclerView)findViewById(R.id.recyclerView_drivers);
+        driversRecycler = (RecyclerView)findViewById(R.id.recyclerView_drivers);
+        driverResponseAdapter = new DriverResponseAdapter(drivers);
+        setupRecyclerView(driverResponseAdapter);
 
+        new Thread(new Runnable() {
+            public void run() {
+                mStompClient = Stomp.over(WebSocket.class, "ws://192.168.1.36:8080/gs-guide-websocket");
+                mStompClient.connect();
+                mStompClient.topic("/topic/client").subscribe(topicMessage -> {
+                    Log.e("PAYLOAD: ",topicMessage.getPayload());
+                    new StompConnectionTask().execute(topicMessage.getPayload());
+                });
+            }
+        }).start();
 
+    }
+
+    public void setupRecyclerView(DriverResponseAdapter adapter){
         driversRecycler.setHasFixedSize(true);
-
         LinearLayoutManager layout = new LinearLayoutManager(getApplicationContext());
         layout.setOrientation(LinearLayoutManager.VERTICAL);
-
-        driversRecycler.setAdapter(new DriverResponseAdapter(buildDrivers()));
-
+        driversRecycler.setAdapter(adapter);
         driversRecycler.setLayoutManager(layout);
     }
 
-    public ArrayList<Driver> buildDrivers(){
-        ArrayList<Driver> drivers = new ArrayList<>();
-        drivers.add(new Driver("John","Pereira Matus","A 5 minutos aprox.","Precio: $16000"));
-        drivers.add(new Driver("Jose","Canto Toro","A 15 minutos aprox.","Precio: $14000"));
-        drivers.add(new Driver("Alejandro","Correa Gonzalez","A 25 minutos aprox.","Precio: $13000"));
-        drivers.add(new Driver("John","Pereira Matus","A 5 minutos aprox.","Precio: $16000"));
-        drivers.add(new Driver("Jose","Canto Toro","A 15 minutos aprox.","Precio: $14000"));
-        drivers.add(new Driver("Alejandro","Correa Gonzalez","A 25 minutos aprox.","Precio: $13000"));
-        drivers.add(new Driver("John","Pereira Matus","A 5 minutos aprox.","Precio: $16000"));
-        drivers.add(new Driver("Jose","Canto Toro","A 15 minutos aprox.","Precio: $14000"));
-        drivers.add(new Driver("Alejandro","Correa Gonzalez","A 25 minutos aprox.","Precio: $13000"));
-        return drivers;
+    public void addItemList(Driver driver){
+        drivers.add(driver);
+        driverResponseAdapter.notifyDataSetChanged();
     }
 
+
+    private class StompConnectionTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return strings[0];
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Toast.makeText(ResponseDriverActivity.this, s, Toast.LENGTH_SHORT).show();
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String precio = jsonObject.getString("content");
+                Driver d = new Driver("Alejandro","Correa Gonzalez","A 25 minutos aprox.","Precio: $"+precio);
+                addItemList(d);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
